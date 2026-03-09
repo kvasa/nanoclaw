f578cd36c0e5539fb83cf23be13acf52084d66ca
Michal Kvasnička <m.kv@snicka.cz>
2026-03-09 20:10:37 +0100

    Prettify

--- src/channels/gmail.ts
+++ src/channels/gmail.ts
@@ -280,9 +280,7 @@ export class GmailChannel implements Channel {
 
     // Find the main group to deliver the email notification
     const groups = this.opts.registeredGroups();
-    const mainEntry = Object.entries(groups).find(
-      ([, g]) => g.isMain === true,
-    );
+    const mainEntry = Object.entries(groups).find(([, g]) => g.isMain === true);
 
     if (!mainEntry) {
       logger.debug(
